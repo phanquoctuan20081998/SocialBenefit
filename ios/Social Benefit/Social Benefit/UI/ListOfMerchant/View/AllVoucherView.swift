2cfaaf8039ec38a1c7af83a81c7d457a67894639
phanquoctuan20081998 <phanquoctuan20081998@gmail.com>
2021-09-16 10:57:14 +0700

    Update 09/16

---
+++ ios/Social Benefit/Social Benefit/UI/ListOfMerchant/View/AllVoucherView.swift
@@ -0,0 +1,139 @@
+//
+//  SpecialOffersView.swift
+//  Social Benefit
+//
+//  Created by Phan Quốc Tuấn on 14/09/2021.
+//
+
+import SwiftUI
+
+struct AllOffersView: View {
+    
+    @ObservedObject var specialOffersViewModel = SpecialOffersViewModel(searchPattern: "", fromIndex: -1, categoryid: -1)
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            Text("all_offer".localized.uppercased())
+                .font(.system(size: 18, weight: .heavy, design: .default))
+                .padding(.leading)
+                .foregroundColor(.orange)
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 15) {
+                    ForEach(self.specialOffersViewModel.allSpecialOffers, id: \.self) { item in
+                    AllOfferCardView(voucherData: item)
+                    }
+                }.padding()
+            }
+        }.environmentObject(specialOffersViewModel)
+    }
+}
+
+struct AllOfferCardView: View {
+    
+    var voucherData: MerchantVoucherItemData
+    
+    var body: some View {
+        HStack {
+            URLImageView(url: voucherData.imageURL)
+                .frame(width: 80, height: 80)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .padding(10)
+                
+            Spacer().frame(width: 5)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                
+                Text("[\(voucherData.merchantName)] \(voucherData.name)")
+                    .font(.system(size: 13))
+                    .lineLimit(2)
+                    .frame(height: 32, alignment: .topLeading)
+
+                
+                LoveAndCartCountView
+                
+                DiscountView
+            }
+        }
+        .frame(width: ScreenInfor().screenWidth * 0.9, height: 100)
+        .padding(5)
+        .background(RoundedRectangle(cornerRadius: 20)
+                        .fill(Color.white)
+                        .shadow(color: .black.opacity(0.2), radius: 10, x: -3, y: 1))
+    }
+}
+
+extension AllOfferCardView {
+    
+    var LoveAndCartCountView: some View {
+        HStack {
+            HStack(spacing: 2) {
+                Image(systemName: "suit.heart")
+                    .foregroundColor(.red)
+                    .font(.system(size: 13))
+                Text("\(voucherData.favoriteValue)")
+                    .font(.system(size: 13))
+            }
+            
+            Rectangle()
+                .fill(Color.gray.opacity(0.5))
+                .frame(width: 2, height: 10)
+                .padding(.horizontal, 5)
+                
+            
+            HStack(spacing: 2) {
+                Image(systemName: "cart")
+                    .foregroundColor(.blue)
+                    .font(.system(size: 13))
+                Text("\(voucherData.shoppingValue)")
+                    .font(.system(size: 13))
+            }
+        }
+        .frame(width: 150, alignment: .leading)
+    }
+    
+    var DiscountView: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("\(voucherData.pointValue) VND")
+                    .bold()
+                    .foregroundColor(.blue)
+                    .font(.system(size: 11))
+                
+                HStack {
+                    Text("\(voucherData.moneyValue) VND")
+                        .strikethrough()
+                        .font(.system(size: 11))
+                    
+                    Text("\(voucherData.discountValue)%")
+                        .bold()
+                        .foregroundColor(.white)
+                        .font(.system(size: 10))
+                        .padding(2)
+                        .background(RoundedRectangle(cornerRadius: 6).fill(Color.orange))
+                }
+            }
+            
+            Spacer()
+            
+            Button(action: {
+                
+                
+            }, label: {
+                Text("buy".localized)
+                    .font(.system(size: 15))
+                    .foregroundColor(.white)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal, 20)
+                    .background(RoundedRectangle(cornerRadius: 10)
+                                    .shadow(color: .black.opacity(0.2), radius: 2, x: -0.5, y: 0.5))
+            })
+        }.padding(.trailing, 20)
+    }
+}
+
+struct AllOffersView_Previews: PreviewProvider {
+    static var previews: some View {
+        AllOffersView()
+    }
+}
---
+++ ios/Social Benefit/Social Benefit/UI/ListOfMerchant/View/VoucherFilterView.swift
@@ -0,0 +1,20 @@
+//
+//  FilterView.swift
+//  Social Benefit
+//
+//  Created by Phan Quốc Tuấn on 16/09/2021.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        FilterView()
+    }
+}
