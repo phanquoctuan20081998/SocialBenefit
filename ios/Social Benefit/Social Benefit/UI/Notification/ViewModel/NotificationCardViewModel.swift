b1d4ca28d7084321b4449acdb618dda61c92f1c4
phanquoctuan20081998 <phanquoctuan20081998@gmail.com>
2022-01-13 23:14:49 +0700

    Update Tuan 13/1

--- ios/Social Benefit/Social Benefit/UI/Notification/ViewModel/NotificationCardViewModel.swift
+++ ios/Social Benefit/Social Benefit/UI/Notification/ViewModel/NotificationCardViewModel.swift
@@ -50,17 +50,19 @@ class NotificationCardViewModel: ObservableObject, Identifiable {
     
     func getContent() {
         if self.notificationItem.getContentParams().isEmpty {
-            self.content = "\(self.notiTypeName): \(self.notificationItem.getContent())"
+            self.content = "<b>\(self.notiTypeName)</b>: \(self.notificationItem.getContent())"
         } else if self.notificationItem.getContentParams().count == 1 {
             
             if self.notificationItem.getContent() == "C00193_I" {
-                self.content = "\(self.notiTypeName): \(self.notificationItem.getContent().localizeWithFormat(arguments: self.notificationItem.getContentParams()[0], self.date))"
+                print(notificationItem.getContent())
+                self.content = "<b>\(self.notiTypeName)</b>: \(self.notificationItem.getContent().localizeWithFormat(arguments: self.notificationItem.getContentParams()[0], self.date))"
             } else {
-                self.content = "\(self.notiTypeName): \(self.notificationItem.getContent().localizeWithFormat(arguments: self.notificationItem.getContentParams()[0]))"
+                print(notificationItem.getContent())
+                self.content = "<b>\(self.notiTypeName)</b>: \(self.notificationItem.getContent().localizeWithFormat(arguments: self.notificationItem.getContentParams()[0]))"
             }
             
         } else {
-            self.content = "\(self.notiTypeName): \(self.notificationItem.getContent().localizeWithFormat(arguments: self.notificationItem.getContentParams()[0], self.notificationItem.getContentParams()[1]))"
+            self.content = "<b>\(self.notiTypeName)</b>: \(self.notificationItem.getContent().localizeWithFormat(arguments: self.notificationItem.getContentParams()[0], self.notificationItem.getContentParams()[1]))"
         }
     }
     
